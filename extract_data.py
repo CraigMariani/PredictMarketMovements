b4bd686d5781ab7a9820bea4e3c7b992a71a7fd2
CraigMariani <craigmariani47@gmail.com>
2021-12-12 22:53:51 -0800

    model is working

--- extract_data.py
+++ extract_data.py
@@ -1,22 +1,31 @@
 from secret import Secret
 from tiingo import TiingoClient
 import pandas as pd 
-from pprint import pprint
 client = TiingoClient(Secret.config)
 from datetime import datetime
 
-start = '2021-01-01'
-end = datetime.now().strftime("%Y-%m-%d")
-symbols = ['VOO', 'SPY', 'IVV', 'SWPPX']
+start_train = '2021-01-01'
+end_train = '2021-11-16'
+
+start_test = '2021-11-16'
+# end_test = datetime.now().strftime("%Y-%m-%d")
+end_test = '2021-12-08'
+
+symbols = ['ETHUSD', 'SHIBUSD', 'BTCUSD', 'DOGEUSD']
 
 for symbol in symbols:
     prices = client.get_ticker_price(
                                     symbol,
                                     fmt='json',
                                     frequency='daily',
-                                    startDate=start,
-                                    endDate=end)
+                                    # startDate=start_train,
+                                    # endDate=end_train,
+                                    startDate=start_test,
+                                    endDate=end_test
+                                    )
+    
     price_sheet = pd.DataFrame.from_dict(prices)
     price_sheet['date'] = price_sheet['date'].str.rstrip('T00:00:00.000Z')
-    print(price_sheet.head())
-    price_sheet.to_csv('data/{}.csv'.format(symbol))
+    # print(price_sheet.head())
+    # price_sheet.to_csv('data/{}.csv'.format(symbol))
+    price_sheet.to_csv('test_data/{}.csv'.format(symbol))
--- secret.py
+++ secret.py
@@ -3,4 +3,4 @@ class Secret:
     config = {}
 
     config['session'] = True
-    config['api_key'] = ''
+    config['api_key'] = 'YOUR API KEY HERE'
